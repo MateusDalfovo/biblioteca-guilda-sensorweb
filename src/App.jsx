a38424f07de46b717da7772b7ad546770e38489f
Francisco Meireles <francisco.alves@sensorweb.com.br>
2026-09-02 16:41:06 -0300

    Botão +

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,55 @@ import Header from './components/Header'
 import Category from './components/Category'
 import materiais from './data/materiais'
 
+import { useState } from 'react'
+
 function App() {
+
+  const [modalAberto, setModalAberto] = useState(false)
 
   const categorias = [...new Set(
     materiais.map((material) => material.categoria)
   )]
 
   return (
+
+
     <div className="app">
 
       <Header />
+
+      <button
+        className="add-button"
+        onClick={() => setModalAberto(true)}
+      >
+        +
+      </button>
 
       <main className="content">
 
         <h2>Materiais disponíveis</h2>
 
+
+        {modalAberto && (
+          <div className="modal-overlay">
+            <div className="modal">
+              <button
+                className="modal-close"
+                onClick={() => setModalAberto(false)}
+              >
+                ×
+              </button>
+
+              Olá! Sou o modal
+            </div>
+          </div>
+        )}
+
         {categorias.map((categoria) => (
           <Category
             key={categoria}
             nome={categoria}
-            icone="📚"
+            icone="✅"
             materiais={materiais.filter(
               (material) => material.categoria === categoria
             )}
